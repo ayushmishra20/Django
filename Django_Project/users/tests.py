57279296c52e0c8e622bc4c83655a7786af9a243
Ayush Mishra <officialayushmishra002@gmail.com>
2026-08-06 13:51:55 +0530

    added password reset through gmail

--- Django_Project/users/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class LogoutPageTests(TestCase):
-    def test_logout_page_renders_for_get_request(self):
-        response = self.client.get('/logout/', HTTP_HOST='localhost')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'You have been logged out')
