5c7146870c3d69ad53dcc6799efcdbe9641acefb
Ayush Mishra <officialayushmishra002@gmail.com>
2026-08-08 08:51:34 +0530

    Django Blog app, basic knowledge of django full app built

--- Django_Project/users/models.py
+++ Django_Project/users/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -13,9 +15,12 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        if not self.image:
+            return
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if os.path.exists(self.image.path):
+            with Image.open(self.image.path) as img:
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
